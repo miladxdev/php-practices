794cf8c558e879343fac13d3f2ccd8af56d1b01f
Milad <miladxgh@outlook.com>
2021-06-27 16:35:33 +0430

    strings

--- string.php
+++ string.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="style.css"> -->
     <title>PHP</title>
     <style>
         body {
@@ -20,7 +19,7 @@
 
         small {
             display: block;
-            margin-top: 10px;
+            margin-top: 20px;
             color: grey;
 
         }
@@ -32,17 +31,18 @@
     <div>
         <?php
         //
-        $header = 'php string';
+        $header = 'php strings';
         echo "<h1> $header </h1>";
 
-        $text = "php string";
+        $text = "php strings";
         echo "<small> length </small> " . strlen($text);
         echo "<small> reverse </small> " . strrev($text);
         echo "<small> positon </small> " . strpos($text, "s");
         echo "<small> word count </small> " . str_word_count($text);
-        echo "<small> replace: </small> " . str_replace("php", "P H P", $text);
+        echo "<small> replace </small> " . str_replace("php", "P H P", $text);
         // echo "<small> str to array </small> " . str_split($text);
         echo "<small> uppercase first char </small> " . ucwords($text);
+        echo "<small> ASCCI to str </small> " . chr(97);
         // 
         ?>
     </div>
